343ae69964836b6d10bc6445cbe466a9ccb03cda
Omer Winter <omerwin@wix.com>
2019-08-15 11:29:47 +0300

    init project

--- ContextHooks/index.js
+++ ContextHooks/index.js
@@ -4,7 +4,6 @@
 
 import { Navigation } from "react-native-navigation";
 import App from './src/App';
-import Players from "./src/screens/players/Players";
 
 Navigation.registerComponent(`navigation.playground.HomeScreen`, () => App);
 
