2807d090c9f9003b7dc6f21dd77e4a6f9b061b84
Omer Winter <omerwin@wix.com>
2019-08-24 12:16:38 +0300

    init project-finish

--- ContextHooks/src/App.js
+++ ContextHooks/src/App.js
@@ -1,9 +1,10 @@
 import React, {useMemo} from 'react';
 import Home from "./screens/Home";
 import {useAppHook} from './hooks/useAppHook';
+
 export const NbaContext = React.createContext();
+
 const App = (props) => {
-  console.log('ttt props', props);
   const {state, contextData} = useAppHook(props);
   return (
     <NbaContext.Provider value={contextData}>
